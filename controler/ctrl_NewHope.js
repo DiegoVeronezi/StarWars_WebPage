53400eaccd4aca74f57d4c58814894d90308d722
DiegoVeronezi <diegoveronezi70@gmail.com>
2018-04-30 18:39:57 -0300

    feat: changes in the method of calling the api

--- controler/ctrl_NewHope.js
+++ controler/ctrl_NewHope.js
@@ -10,29 +10,21 @@ APP.controller('IndexCtrl', function ($scope, $http) {
     }, function errorCallback(response) {
       // called asynchronously if an error occurs
       // or server returns response with an error status.
-    });	
-  $http({
-    method: 'GET',
-    url: 'https://swapi.co/api/people/1/'
-    }).then(function successCallback(response) {
-      console.log(response);
-      $scope.person1 = response;
-      // this callback will be called asynchronously
-      // when the response is available
-    }, function errorCallback(response) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
     });
-  $http({
-    method: 'GET',
-    url: 'https://swapi.co/api/people/4/?format=json'
-    }).then(function successCallback(response) {
-      console.log(response);
-      $scope.person2 = response;
-      // this callback will be called asynchronously
-      // when the response is available
-    }, function errorCallback(response) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-    });	  	  			
-});	
+  function callApiForPeople(id) {
+    $http({
+      method: 'GET',
+      url: 'https://swapi.co/api/people/' + id + '/',
+      }).then(function successCallback(response) {
+        console.log(response);
+        $scope['person' + id] = response;
+        // this callback will be called asynchronously
+        // when the response is available
+      }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
+  }
+  callApiForPeople(1);
+  callApiForPeople(2);
+});
