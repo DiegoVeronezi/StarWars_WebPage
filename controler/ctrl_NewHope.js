42bccf940ba535c917fdea0684e89d14df5fd69d
DiegoVeronezi <diegoveronezi70@gmail.com>
2018-05-02 00:04:49 -0300

    feat: changes in the layout of the movie and receiving the characters from the api

--- controler/ctrl_NewHope.js
+++ controler/ctrl_NewHope.js
@@ -1,23 +1,29 @@
 APP.controller('IndexCtrl', function ($scope, $http) {
+  $scope.person = [];
+
   $http({
     method: 'GET',
-    url: 'https://swapi.co/api/films/1/?format=json'
+    url: 'https://swapi.co/api/films/'
     }).then(function successCallback(response) {
-      console.log(response);
-      $scope.film1 = response;
+      for (let i = 0; i < response.data.results.length; i++) {
+        $scope['films' + i] = response.data.results[i];        
+      }
+      console.log($scope.films0);
       // this callback will be called asynchronously
       // when the response is available
     }, function errorCallback(response) {
       // called asynchronously if an error occurs
       // or server returns response with an error status.
     });
+
   function callApiForPeople(id) {
     $http({
       method: 'GET',
       url: 'https://swapi.co/api/people/' + id + '/',
       }).then(function successCallback(response) {
-        console.log(response);
-        $scope['person' + id] = response;
+         
+         $scope.person[id] = response;
+         console.log($scope.person[1]);
         // this callback will be called asynchronously
         // when the response is available
       }, function errorCallback(response) {
@@ -25,6 +31,15 @@ APP.controller('IndexCtrl', function ($scope, $http) {
         // or server returns response with an error status.
       });
   }
-  callApiForPeople(1);
-  callApiForPeople(2);
+
+  function getCharacters() {
+    for (let i = 1; i < 5; i++) {
+      callApiForPeople([i]);     
+    }
+    
+    
+  }
+
+  getCharacters();
+ 
 });
